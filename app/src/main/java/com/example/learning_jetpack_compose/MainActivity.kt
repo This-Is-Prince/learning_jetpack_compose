39968e056969204290a719339ed331c1417660c8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-15 21:46:57 +0530

    feat: ListViewItem composable added

--- app/src/main/java/com/example/learning_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/learning_jetpack_compose/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
@@ -16,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -24,6 +22,7 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -43,11 +42,28 @@ private fun Setup() {
 @Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
 private fun PreviewFunction() {
-    Box(
-        contentAlignment = Alignment.Center
-    ){
-        Text(text = "A", fontSize = 24.sp)
-        Text(text = "B", fontSize = 24.sp)
+    ListViewItem(R.drawable.cat_quote, "John Doe","Software Developer")
+}
+
+@Composable
+fun ListViewItem(imgId: Int, name: String, occupatioin: String) {
+    Row {
+        Image(
+            painter = painterResource(id = imgId),
+            contentDescription = "Quote with cat",
+            Modifier.size(100.dp)
+        )
+        Column{
+            Text(
+                text = name,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = occupatioin,
+                fontWeight = FontWeight.Thin,
+                fontSize = 12.sp
+            )
+        }
     }
 }
 
