3ecadb2e329f4386470f63dd01029d6ba13c56c0
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-16 07:01:33 +0530

    feat: stateless and statefull composable added

--- app/src/main/java/com/example/learning_jetpack_compose/StateExample.kt
+++ app/src/main/java/com/example/learning_jetpack_compose/StateExample.kt
@@ -1,17 +1,21 @@
 package com.example.learning_jetpack_compose
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -21,17 +25,37 @@ fun NotificationScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize(1f)
     ) {
-        NotificationCounter()
+        val count = rememberSaveable{mutableStateOf(0)}
+        NotificationCounter(count.value) { count.value++ }
+        MessageBar(count.value)
     }
 }
 
 @Composable
-fun NotificationCounter() {
-    val count = rememberSaveable{mutableStateOf(0)}
+fun MessageBar(count: Int) {
+    Card (
+        elevation = 4.dp
+    ) {
+        Row(
+            Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                imageVector = Icons.Outlined.Favorite,
+                contentDescription = "",
+                Modifier.padding(4.dp)
+            )
+            Text(text = "Messages sent so far - $count")
+        }
+    }
+}
+
+@Composable
+fun NotificationCounter(count: Int, increment: () -> Unit) {
     Column() {
-        Text(text = "You have sent ${count.value} notifications")
+        Text(text = "You have sent $count notifications")
         Button(onClick = {
-            count.value++
+            increment()
             Log.d("CODERSTAG", "Button Clicked")
         }) {
             Text(text = "Send Notification")
